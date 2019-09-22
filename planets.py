c2f3bafdb9562b4f72a3173a824e1abda70939b2
= <=>
2019-09-21 19:28:28 -0700

    Rieterk, Sarah Lab0

--- planets.py
+++ planets.py
@@ -1,7 +1,21 @@
+# Student: Sarah Rietkerk
+# Instructor: Paul Hatalsky
+# Class: CPE 202
+# Term: Fall 2019
+
+
 def weight_on_planets():
-   # write your code here
-   
+    #get someone's weight and make it into a float for multiplication
+    earth_weight = float(input("What do you weigh on earth? "))
+    #calculate weight on mars by multiplying Earth weight by 0.38
+    mars = earth_weight * 0.38
+    #calculate weight on jupiter by multiplying Earth weight by 2.34
+    jupiter = earth_weight * 2.34
+    #display the results and place the float value of both weights at the 
+    # location of the curly brackets, limited to 2 decimal places
+    print("\nOn Mars you would weigh {:.2f} pounds.".format(mars) +
+        "\nOn Jupiter you would weigh {:.2f} pounds.".format(jupiter))
    
    
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
